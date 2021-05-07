638a0d7f5caa42f06d2a0cb5fb882bf71911ac05
anbecodes <anbecodes@gmail.com>
2021-05-07 20:42:11 +0200

    delete all files

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// home
-router.get('/', (req, res) => {
-    res.render('index.html', { title: 'Converter'});
-});
-
-module.exports = router;
